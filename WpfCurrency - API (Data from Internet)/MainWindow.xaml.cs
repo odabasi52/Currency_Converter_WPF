d2bf307fa67b462e162355b36b69b4bd18bfd973
mtosuruk52 <104325179+mtosuruk52@users.noreply.github.com>
2022-08-25 21:13:54 +0300

    WPF
    Currency Converter WPF

---
+++ WpfCurrency - API (Data from Internet)/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Threading;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json; //Install from Nuget Manager
+
+namespace WpfCurrency
+{
+    public partial class MainWindow : Window
+    {
+        /*If you want to get data from internet you have to use exact same names as the site which
+        you get GET response then convert it to .json file and store it as Root object*/
+        public class Root
+        {
+            public Currencies rates { get; set; } 
+            //In the site I used, rates are Currencies as double vallue so I give the name exactly same.
+        }
+
+        public class Currencies //You can add more currencies with same name from url.
+        {
+            public double USD { get; set; }
+            public double EUR { get; set; }
+            public double TRY { get; set; }
+            public double AED { get; set; }
+            public double CAD { get; set; }
+            public double RUB { get; set; }
+            public double KWD { get; set; }
+            public double AUD { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CurrencyText.Focus();
+            GetValue();
+        }
+
+        #region First get GET response from url and return it as Root Object
+        //with async, code can work parallel. It does not have to wait.
+        public static async Task<Root> GetData(string url)
+        {
+            Root myRoot = new Root();
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.Timeout = TimeSpan.FromSeconds(30); //wait 30 seconds to get GET response else timeout
+                    HttpResponseMessage response = await client.GetAsync(url); //GET response (from url)
+                    if (response.StatusCode == HttpStatusCode.OK) //If GET response is gotten from url the status code will be 200(OK)
+                    {
+                        string ResponseString = await response.Content.ReadAsStringAsync(); //turn GET response content to a string
+                        Root ResponseObject = JsonConvert.DeserializeObject<Root>(ResponseString); //convert string to .json then read it as Root object
+                        return ResponseObject;
+                    }
+                    return myRoot;
+                }
+            } catch { return myRoot; }
+        }
+        #endregion
+
+        Root value = new Root();
+        async void GetValue()
+        {
+            value = await GetData("https://openexchangerates.org/api/latest.json?app_id=eed04fa1b50e414783fad7bd9b14898e"); //await to get resualt as Root object
+            CurrencyTable(); // after value is assigned get Currency Table
+        }
+
+        #region Set ComboBoxes Source with Values from Internet
+        void CurrencyTable()
+        {
+            DataTable currencies = new DataTable();
+
+            currencies.Columns.Add("Name");
+            currencies.Columns.Add("Value");
+
+            currencies.Rows.Add("-------SELECT-------", 0);
+            currencies.Rows.Add("USD", value.rates.USD);
+            currencies.Rows.Add("EUR", value.rates.EUR);
+            currencies.Rows.Add("TRY", value.rates.TRY);
+            currencies.Rows.Add("AED", value.rates.AED);
+            currencies.Rows.Add("CAD", value.rates.CAD);
+            currencies.Rows.Add("RUB", value.rates.RUB);
+            currencies.Rows.Add("KWD", value.rates.KWD);
+            currencies.Rows.Add("AUD", value.rates.AUD);
+
+            cBoxFrom.DisplayMemberPath = "Name";
+            cBoxFrom.SelectedValuePath = "Value";
+            cBoxFrom.ItemsSource = currencies.DefaultView;
+            cBoxFrom.SelectedIndex = 0;
+
+            cBoxTo.DisplayMemberPath = "Name";
+            cBoxTo.SelectedValuePath = "Value";
+            cBoxTo.ItemsSource = currencies.DefaultView;
+            cBoxTo.SelectedIndex = 0;
+        }
+        #endregion
+
+        #region CheckConversionThenConvert
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            double ConvertedValue;
+
+            if (CurrencyText.Text == null || CurrencyText.Text.Trim() == "")
+            {
+                MessageBox.Show("Please Enter Currency", "INFO", MessageBoxButton.OK, MessageBoxImage.Information);
+                CurrencyText.Focus(); //after messagebox gone focus keyboard to text box
+            }
+
+            else if ((cBoxFrom.SelectedItem == null || cBoxFrom.SelectedIndex == 0) || (cBoxTo.SelectedItem == null || cBoxTo.SelectedIndex == 0))
+            {
+                MessageBox.Show("Select Currency", "INFO", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            #region CurrencyCalculation
+            else
+            {
+                string number = CurrencyText.Text;
+
+                if (double.TryParse(number, out ConvertedValue))
+                {
+                    if (ConvertedValue < 0)
+                    {
+                        MessageBox.Show("Currency can not have negative value", "INFO", MessageBoxButton.OK, MessageBoxImage.Information);
+                        CurrencyText.Focus();
+                    }
+
+                    else
+                    {
+                        ConvertedValue = ConvertedValue * (double.Parse(cBoxTo.SelectedValue.ToString()) / double.Parse(cBoxFrom.SelectedValue.ToString()));
+                        ConvertedLabel.Content = cBoxTo.Text + "  " + ConvertedValue.ToString("N3");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Enter correct form of currency", "INFO", MessageBoxButton.OK, MessageBoxImage.Information);
+                    CurrencyText.Focus();
+                }
+            }
+            #endregion
+        }
+        #endregion
+
+        #region Clear TextBox and Conversions
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            cBoxFrom.SelectedIndex = 0;
+            cBoxTo.SelectedIndex = 0;
+            ConvertedLabel.Content = "00.000";
+            CurrencyText.Text = "";
+            CurrencyText.Focus();
+        }
+        #endregion
+
+    }
+}
---
+++ WpfCurrency - SQLDatabase/MainWindow.xaml.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WpfCurrency
+{
+    public partial class MainWindow : Window
+    {
+        SqlConnection connection;
+
+        int CurrencyId = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["WpfCurrency.Properties.Settings.ConnectionString"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+
+            BindCurrency();
+            ShowDataGrid();
+        }
+
+        #region Clear TextBox and Conversions
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            cBoxFrom.SelectedIndex = 0;
+            cBoxTo.SelectedIndex = 0;
+            ConvertedLabel.Content = "00.000";
+            CurrencyText.Text = "";
+            CurrencyText.Focus();
+        }
+        #endregion
+
+        #region CheckConversionThenConvert
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            double ConvertedValue;
+
+            if(CurrencyText.Text == null || CurrencyText.Text.Trim() == "")
+            {
+                MessageBox.Show("Please Enter Currency", "INFO", MessageBoxButton.OK, MessageBoxImage.Information);
+                CurrencyText.Focus(); //after messagebox gone focus keyboard to textbox
+            }
+            
+            else if((cBoxFrom.SelectedItem == null || cBoxFrom.SelectedIndex == 0) || (cBoxTo.SelectedItem == null || cBoxTo.SelectedIndex == 0))
+            {
+                MessageBox.Show("Select Currency", "INFO", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            #region CurrencyCalculation
+            else
+            {
+                string number = CurrencyText.Text;
+
+                if (double.TryParse(number, out ConvertedValue))
+                {
+                    if(ConvertedValue < 0)
+                    {
+                        MessageBox.Show("Currency can not have negative value", "INFO", MessageBoxButton.OK, MessageBoxImage.Information);
+                        CurrencyText.Focus();
+                    }
+
+                    else
+                    {
+                        ConvertedValue = ConvertedValue * (double.Parse(cBoxTo.SelectedValue.ToString()) / double.Parse(cBoxFrom.SelectedValue.ToString()));
+                        ConvertedLabel.Content = cBoxTo.Text + "  " + ConvertedValue.ToString("N3");
+                    }                   
+                }
+                else
+                {
+                    MessageBox.Show("Enter correct form of currency", "INFO", MessageBoxButton.OK, MessageBoxImage.Information);
+                    CurrencyText.Focus();
+                }
+            }
+            #endregion
+        }
+        #endregion
+
+        #region Create DataTable for ComboBoxes (Currency Selection)
+        void BindCurrency()
+        {
+            
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from CurrencyMaster ", connection);
+            
+            using(adapter)
+            {
+                DataTable currencies = new DataTable();
+                adapter.Fill(currencies);
+
+                DataRow newRow = currencies.NewRow(); //create new row
+                newRow["Id"] = 0; //make first row SELECT (always)
+                newRow["CurrencyName"] = "-------SELECT-------";
+                currencies.Rows.InsertAt(newRow, 0);
+
+                if(currencies.Rows.Count > 0)
+                {
+                    cBoxFrom.ItemsSource = currencies.DefaultView;
+                    cBoxTo.ItemsSource = currencies.DefaultView;
+
+
+                    cBoxFrom.DisplayMemberPath = "CurrencyName";
+                    cBoxFrom.SelectedValuePath = "Amount";
+
+                    cBoxTo.DisplayMemberPath = "CurrencyName";
+                    cBoxTo.SelectedValuePath = "Amount";
+                }
+            }         
+
+            cBoxFrom.SelectedIndex = 0;
+            cBoxTo.SelectedIndex = 0;
+        }
+        #endregion
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtAmount.Text.Trim() == "")
+                {
+                    MessageBox.Show("Enter an amount please", "NO AMOUNT", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    txtAmount.Focus();
+                }
+                else if (txtCurrencyName.Text.Trim() == "")
+                {
+                    MessageBox.Show("Enter Currency Name please", "NO NAME", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    txtCurrencyName.Focus();
+                }
+                else
+                {
+                    if (CurrencyId != 0)
+                    {
+                        if (MessageBox.Show("You sure to update?", "UPDATE", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                        {
+                            SqlCommand command = new SqlCommand("UPDATE CurrencyMaster SET Amount = @Amount, CurrencyName = @CurrencyName WHERE Id = @Id", connection);
+                            connection.Open();
+                            command.Parameters.AddWithValue("@Id", CurrencyId);
+                            command.Parameters.AddWithValue("@Amount", txtAmount.Text);
+                            command.Parameters.AddWithValue("@CurrencyName", txtCurrencyName.Text);
+                            command.ExecuteNonQuery();
+                            connection.Close();
+
+                            MessageBox.Show("Data succesfully updated", "UPDATE", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                    }
+                    else
+                    {
+                        if (MessageBox.Show("You sure to save?", "SAVE", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                        {
+                            SqlCommand command = new SqlCommand("insert into CurrencyMaster(Amount,CurrencyName) Values(@Amount,@CurrencyName)", connection);
+                            connection.Open();
+                            command.Parameters.AddWithValue("@Amount", txtAmount.Text);
+                            command.Parameters.AddWithValue("@CurrencyName", txtCurrencyName.Text);
+                            command.ExecuteNonQuery();
+                            connection.Close();
+
+                            MessageBox.Show("Data succesfully saved", "SAVE", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                    }
+                }
+
+                txtAmount.Text = "";
+                txtCurrencyName.Text = "";
+                btnSave.Content = "Save";
+                CurrencyId = 0;
+                ShowDataGrid();
+                BindCurrency();
+            }
+            catch {}
+        }
+
+        void ShowDataGrid()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from CurrencyMaster", connection);
+
+            using(adapter)
+            {
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                dgvCurrency.ItemsSource = table.DefaultView;
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            txtAmount.Text = "";
+            txtCurrencyName.Text = "";
+            btnSave.Content = "Save";
+            CurrencyId = 0;
+            ShowDataGrid();
+            BindCurrency();
+        }
+
+        private void dgvCurrency_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            try
+            {
+                DataGrid grid = dgvCurrency;
+                DataRowView row = grid.CurrentItem as DataRowView; //get the selected cell with values
+                if(row != null)
+                {
+                    CurrencyId = int.Parse(row["Id"].ToString());
+
+                    if (grid.SelectedCells[0].Column.DisplayIndex == 0)
+                    {
+                        btnSave.Content = "Update";
+                        txtAmount.Text = row["Amount"].ToString();
+                        txtCurrencyName.Text = row["CurrencyName"].ToString();
+                    }
+                    else if (grid.SelectedCells[0].Column.DisplayIndex == 1)
+                    {
+                        if (MessageBox.Show("You Really Wanna Delete?", "DELETE", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                        {
+                            SqlCommand command = new SqlCommand("delete from CurrencyMaster where Id = @Id", connection);
+                            connection.Open();
+                            command.Parameters.AddWithValue("@Id", CurrencyId);
+                            command.ExecuteNonQuery();
+                            ShowDataGrid();
+                            BindCurrency();
+                            connection.Close();
+                        }
+                    }
+                    else
+                    {
+                        txtAmount.Text = "";
+                        txtCurrencyName.Text = "";
+                        btnSave.Content = "Save";
+                        ShowDataGrid();
+                        BindCurrency();
+                    }
+                }
+                
+            }
+            catch {}
+        }
+    }
+}
